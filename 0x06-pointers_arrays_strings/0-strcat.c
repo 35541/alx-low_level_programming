4565ff34212a978802f2a1d2efdf4b827e54a23d
alexandernnaji <nnajialexander@gmail.com>
2022-04-22 16:29:21 -0700

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- * _strcat - concatenates two strings
- * @dest: A pointer to a character that will be changed
- * @src: A pointer to a character that will also bie changed
- * Return: dest
+ *_strcat - concatenates two strings
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will also bie changed
+ *Return: dest
  */
 
 char *_strcat(char *dest, char *src)
